ec4bab5837f41f602242ded885f3fc18526672a1
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-03-05 16:30:57 +0100

    use list as filter to allow for many datasets

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -2,6 +2,8 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
   primary: themeStyle.colors.primary
 }});
 
+var datasets = ['e61dbcec-5082-4a20-9d15-0be61dac7720'];
+
 var siteConfig = {
   routes: {
     enabledRoutes: ['occurrenceSearch']
@@ -16,7 +18,7 @@ var siteConfig = {
     // for his demo site we only show Fungi (taxonKey=5). It use the predicate structure known from GBIF download API. 
     // See https://www.gbif.org/developer/occurrence (long page without enough anchors - search for "Occurrence Download Predicates")
     // The format is however slightly different, in that is use camelCase for keys instead of CONSTANT_CASE. 
-    rootPredicate: { type: 'equals', key: 'datasetKey', value: 'e61dbcec-5082-4a20-9d15-0be61dac7720' }, 
+    rootPredicate: { type: 'in', key: 'datasetKey', values: datasets }, 
     occurrenceSearchTabs: ['TABLE', 'MAP', 'GALLERY', 'DATASETS', 'CLUSTERS', 'DASHBOARD'], // what tabs should be shown
     excludedFilters: ['occurrenceStatus', 'networkKey', 'hostingOrganizationKey', 'protocol', 'publishingCountryCode', 'institutionCode', 'collectionCode'],
     highlightedFilters: ['taxonKey', 'verbatimScientificName', 'institutionKey', 'collectionKey', 'catalogNumber', 'recordedBy', 'identifiedBy'],
