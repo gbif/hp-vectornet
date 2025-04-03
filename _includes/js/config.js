31c58042e003ddd8dbea6bfae1b34735829d3627
Tim Robertson <timrobertson100@gmail.com>
2025-04-03 20:52:44 +0200

    Added 4 datasets as requested via PS

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -2,7 +2,7 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
   primary: themeStyle.colors.primary
 }});
 
-var datasets = ['e61dbcec-5082-4a20-9d15-0be61dac7720'];
+var datasets = ['e61dbcec-5082-4a20-9d15-0be61dac7720', '1c3d0e60-90fe-48e2-9536-e3b0583c4172', 'af2e74e4-bd2e-4983-97ac-e21f6cc430a0', '4abd984b-122c-44a0-8c92-b37e2f5299b1', '94ffb88e-d385-4846-888f-038edba9fff6'];
 
 var siteConfig = {
   routes: {
