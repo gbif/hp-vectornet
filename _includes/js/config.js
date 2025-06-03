38b3ed435772504c813e7b9db12aedebcccbcdbe
Tim Robertson <timrobertson100@gmail.com>
2025-06-03 20:12:23 +0200

    New datasets provided by PS

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -2,7 +2,13 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
   primary: themeStyle.colors.primary
 }});
 
-var datasets = ['e61dbcec-5082-4a20-9d15-0be61dac7720', '1c3d0e60-90fe-48e2-9536-e3b0583c4172', 'af2e74e4-bd2e-4983-97ac-e21f6cc430a0', '4abd984b-122c-44a0-8c92-b37e2f5299b1', '94ffb88e-d385-4846-888f-038edba9fff6', '02e8ee36-9f83-4850-a0f2-edcaacb80818', 'fc1dd9e3-ff53-4d57-9d8e-e29afbb955b8', '88da84d8-6c33-4012-828a-cdf738e542b9', 'd7add022-2594-4816-9c37-bc50fa8c9a61'];
+var datasets = [
+  'e61dbcec-5082-4a20-9d15-0be61dac7720', '1c3d0e60-90fe-48e2-9536-e3b0583c4172', 'af2e74e4-bd2e-4983-97ac-e21f6cc430a0', 
+  '4abd984b-122c-44a0-8c92-b37e2f5299b1', '94ffb88e-d385-4846-888f-038edba9fff6', '02e8ee36-9f83-4850-a0f2-edcaacb80818', 
+  'fc1dd9e3-ff53-4d57-9d8e-e29afbb955b8', '88da84d8-6c33-4012-828a-cdf738e542b9', 'd7add022-2594-4816-9c37-bc50fa8c9a61',
+  '4785284d-b827-4d9e-95db-de23d527967c', 'bc28777c-2da4-4a65-9810-7389cbf62834', '5ed74de7-0ea3-4304-a4d5-882f129369b5', 
+  '8d870547-a0b3-4e90-9c8f-f9465967dd9d', '48c0f475-61e6-4543-9cab-c67fae9000e2', '04939588-db37-4806-bb09-a95886f7741e'
+];
 
 var siteConfig = {
   routes: {
